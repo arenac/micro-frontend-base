a097b026f5febd28c4d3e22018e5e7c69a5663b4
Nilo Neregato <neregato.nilo@gmail.com>
2020-06-04 22:21:10 +0200

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+module.exports = {
+  mode: process.env.NODE_ENV,
+  output: {
+    filename: 'bundle.js',
+    publicPath: isProduction ? './' : '/cdn/base/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-react'],
+            },
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      __DISCOVERY_BASE_URL__: isProduction ? '"/"' : '"/cdn/"',
+    }),
+    new HtmlWebpackPlugin(),
+  ],
+  optimization: {
+    minimize: false,
+  },
+};
